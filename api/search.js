51ae79e3cd206ae0f75fe9d60a410de6ccebf047
ebuka00q-ui <ebuka00q@gmail.com>
2026-07-31 15:26:00 +0100

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,1 +1,19 @@
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
 
+    if (req.method !== "GET") {
+        return res.status(405).json({
+            success: false,
+            message: "Method not allowed."
+        });
+    }
+
+    const query = req.query.q || "";
+
+    return res.status(200).json({
+        success: true,
+        query,
+        results: []
+    });
+}
