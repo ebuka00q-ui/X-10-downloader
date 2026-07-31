4c3f32cc55d015ed347dfcd49163195bfa19f02e
ebuka00q-ui <ebuka00q@gmail.com>
2026-07-31 14:37:59 +0100

    Update history.js

--- api/history.js
+++ api/history.js
@@ -1,1 +1,16 @@
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
 
+    if (req.method !== "GET") {
+        return res.status(405).json({
+            success: false,
+            message: "Method Not Allowed"
+        });
+    }
+
+    return res.status(200).json({
+        success: true,
+        history: []
+    });
+}
