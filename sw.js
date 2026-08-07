fca90a20f7c5a4fbeb4d2521cd31dc524a10c80e
ebuka00q-ui <ebuka00q@gmail.com>
2026-08-07 20:43:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'x10-downloader-v1';
+const CACHE_NAME = 'x10-downloader-v2';
 
 const CACHE_ASSETS = [
   '/',
@@ -7,15 +7,34 @@ const CACHE_ASSETS = [
   '/file_0000000074e081f681d168ece0276989~2.jpg'
 ];
 
-
+// Install Event
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
+      return cache.addAll(CACHE_ASSETS);
     })
   );
 });
 
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    clients.claim().then(() => {
+      return caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== CACHE_NAME) {
+              return caches.delete(cache);
+            }
+          })
+        );
+      });
+    })
+  );
+});
+
+// Fetch Event
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
@@ -23,3 +42,4 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+    
